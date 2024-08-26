2a150343e43f45889aa815bdad939ae7bff0b962
kevlu28 <kevin_luza@usmp.pe>
2024-08-26 00:52:06 -0500

    Add files via upload

---
+++ tienda.js
@@ -0,0 +1,4 @@
+document.getElementById('contactoForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por contactarnos. Te responderemos pronto.');
+});
